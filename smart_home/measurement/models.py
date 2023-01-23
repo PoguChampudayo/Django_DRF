5a5c0372050b349734a7b8230b8e97956ac92296
Aleksey Gordeyev <agordeyev94@gmail.com>
2023-01-23 22:13:16 +0500

    Second try

--- smart_home/measurement/models.py
+++ smart_home/measurement/models.py
@@ -5,7 +5,7 @@ class Sensor(models.Model):
     description = models.CharField(max_length=100)
     
 class TemperatureMeasurement(models.Model):
-    sensor_id = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements')
+    sensor_id = models.ForeignKey(Sensor, to_field='id', on_delete=models.CASCADE)
     temperature = models.FloatField()
     measurement_date = models.DateField(auto_now_add=True)
     
